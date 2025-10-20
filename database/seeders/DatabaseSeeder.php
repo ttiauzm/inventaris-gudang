1995ec2c71b031e641350cb7c209dd5c5a8d17d8
tiauzm <mutiaumniatizupri@mail.ugm.ac.id>
2025-10-20 21:12:44 +0700

    Category, material, item seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,9 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RoleSeeder::class,
             UserSeeder::class,
+            CategorySeeder::class,
+            MaterialSeeder::class,
+            ItemSeeder::class,
         ]);
     }
 }

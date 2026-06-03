ea219483aab89f16f8293bf9d1f27f1f52b702d0
ttiauzm <mutiaumniatizupri@mail.ugm.ac.id>
2026-06-03 18:36:54 +0700

    feat: add route get faulty list

--- routes/api.php
+++ routes/api.php
@@ -48,6 +48,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/items/{id}/qrcode', [ItemController::class, 'generateQr']);
     Route::post('/items/{id}/faulty', [ItemController::class, 'reportFaulty']);
     Route::get('/items/history/child', [ItemController::class, 'getChildItems']);
+    Route::get('/items/history/faulty', [ItemController::class, 'getFaultyList']);
 
     Route::get('/transactions', [TransactionController::class, 'index']);
     Route::get('/transactions/{id}', [TransactionController::class, 'show']);

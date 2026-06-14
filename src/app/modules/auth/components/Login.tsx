b35f15c721e22909853bced7a8e49eff4fe74d25
Ryzen0000001 <rizendyaffarin@mail.ugm.ac.id>
2026-06-14 19:52:14 +0700

    fix: update axios base url to https and add /api prefix

--- src/app/modules/auth/components/Login.tsx
+++ src/app/modules/auth/components/Login.tsx
@@ -457,12 +457,12 @@ export function Login() {
         </button>
       </div>
 
-      <div className='text-gray-500 text-center fw-semibold fs-6'>
+      {/* <div className='text-gray-500 text-center fw-semibold fs-6'>
         Not a Member yet?{' '}
         <Link to='/auth/registration' className='link-primary'>
           Sign up
         </Link>
-      </div>
+      </div> */}
     </form>
   )
 }
